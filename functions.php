ae5db43737c4ac0b53195709cbff6c5a5650bf5a
SBonzi <stefano.bonzi@siag.it>
2023-06-29 16:30:54 +0200

    namespace again

--- functions.php
+++ functions.php
@@ -1,17 +1,16 @@
 ﻿<?php
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-	$parenthandle = 'twenttwentyone-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+	$parenthandle = 'twentywentone-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 	$theme        = wp_get_theme();
 	wp_enqueue_style( $parenthandle,
 		get_template_directory_uri() . '/style.css',
 		array(),  // If the parent theme code has a dependency, copy it to here.
 		$theme->parent()->get( 'Version' )
 	);
-	wp_enqueue_style( 'sbonzix-style',
+	wp_enqueue_style( 'twentywentone-child-style',
 		get_stylesheet_uri(),
 		array( $parenthandle ),
 		$theme->get( 'Version' ) // This only works if you have Version defined in the style header.
 	);
 }
-?>
