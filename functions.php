a07880d28fb6b95621bd2cef627208ff23277ce5
SBonzi <stefano.bonzi@siag.it>
2023-06-29 16:21:06 +0200

    changed functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,16 @@
 ﻿<?php
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-	wp_enqueue_style( 'twentytwentyonechild-style',
+	$parenthandle = 'twenttwentyone-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+	$theme        = wp_get_theme();
+	wp_enqueue_style( $parenthandle,
+		get_template_directory_uri() . '/style.css',
+		array(),  // If the parent theme code has a dependency, copy it to here.
+		$theme->parent()->get( 'Version' )
+	);
+	wp_enqueue_style( 'child-style',
 		get_stylesheet_uri(),
-		array( 'parenthandle' ),
-		wp_get_theme()->get( 'Version' ) // This only works if you have Version defined in the style header.
+		array( $parenthandle ),
+		$theme->get( 'Version' ) // This only works if you have Version defined in the style header.
 	);
 }
