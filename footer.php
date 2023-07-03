166b155a33eb0c53f513d00874e65df5161eb95c
SBonzi <stefano.bonzi@siag.it>
2023-07-03 16:12:02 +0200

    first theme structure

---
+++ footer.php
@@ -0,0 +1,12 @@
+﻿<?php
+/**
+ *
+ * @package sbonzix2023
+ */
+?>
+		</main>
+		<footer>
+		</footer>
+		<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,25 @@
+﻿<?php
+/**
+ *
+ * @package sbonzix2023
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+	<header>
+		<h1><?php bloginfo('name'); ?></h1>
+		<!--<h2><?php bloginfo('description'); ?></h2>-->
+	</header>
+	<main>
+		<nav>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="navbar-brand"><?php bloginfo( 'name' ); ?></a>
+			<ul class="nav navbar-nav navbar-right">
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'items_wrap' => '%3$s' ) ); ?>
+			</ul>
+		</nav>
+  <main>
