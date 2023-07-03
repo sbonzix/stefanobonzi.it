fe95309855d16602c037862d33399920b5acecbe
SBonzi <stefano.bonzi@siag.it>
2023-07-03 13:14:56 +0200

    added the loop

--- index.php
+++ index.php
@@ -16,14 +16,17 @@
 		<!--<h2><?php bloginfo('description'); ?></h2>-->
 	</header>
 	<main>
-		<?php
-			if ( have_posts() ) :
-				/* Start the Loop */
-				while ( have_posts() ) :
-					the_post();
-				endwhile;
-			endif;
-		?>
+	<?php
+    if ( have_posts() ) : while ( have_posts() ): the_post(); ?>
+
+    <div id="post-<?php the_ID(); ?>">
+        <h2><?php the_title(); ?></h2>
+        <div class="post-excerpt"><?php the_excerpt(); ?></div>
+    </div>
+
+    <?php endwhile;
+    endif;
+    ?>
 	</main>
 	<footer>
 	</footer>
