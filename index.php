b240c0775954026a1a321cea3ce9c763c454c43d
SBonzi <stefano.bonzi@siag.it>
2023-07-03 17:12:30 +0200

    corrected index

--- index.php
+++ index.php
@@ -6,14 +6,9 @@
 ?><?php get_header(); ?>
 
 <?php
-    if ( have_posts() ) : while ( have_posts() ): the_post(); ?>
-
-    <div id="post-<?php the_ID(); ?>">
-        <h2><?php the_title(); ?></h2>
-        <div class="post-excerpt"><?php the_excerpt(); ?></div>
-    </div>
-
-    <?php endwhile;
-    endif;
-    ?>
+  if ( have_posts() ) : while ( have_posts() ): the_post();
+    get_template_part( 'partials/content', get_post_type() );
+  endwhile;
+	endif;
+?>
 <?php get_footer(); ?>
