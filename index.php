b710c8dc8fbb69d7b0a56a83703367d6ea3b26e4
SBonzi <stefano.bonzi@siag.it>
2023-07-03 13:03:58 +0200

    restructured repo

---
+++ index.php
@@ -0,0 +1,30 @@
+﻿<?php
+/**
+ *
+ * @package sbonzix2023
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<title><?php bloginfo('name'); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	<?php wp_head(); ?>
+</head>
+<body>
+	<header>
+		<h1><?php bloginfo('name'); ?></h1>
+		<h2><?php bloginfo('description'); ?></h2>
+	</header>
+	<main>
+		<?php
+			if ( have_posts() ) :
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
+				endwhile;
+			endif;
+		?>
+	</main>
+	<footer>
+	</footer>
+</body>
