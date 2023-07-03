36fcb2c8110d1a67cbf1d3874d635bf9a857a7ee
SBonzi <stefano.bonzi@siag.it>
2023-07-03 17:15:54 +0200

    again

--- index.php
+++ index.php
@@ -5,10 +5,13 @@
  */
 ?><?php get_header(); ?>
 
+if ( have_posts() ) : while ( have_posts() ): the_post(); ?>
+	<div id="post-<?php the_ID(); ?>">
+		<h2><?php the_title(); ?></h2>
+		<div class="post-excerpt"><?php the_excerpt(); ?></div>
+	</div>
 <?php
-  if ( have_posts() ) : while ( have_posts() ): the_post();
-    get_template_part( 'partials/content', get_post_type() );
-  endwhile;
+	endwhile;
 	endif;
 ?>
 <?php get_footer(); ?>
